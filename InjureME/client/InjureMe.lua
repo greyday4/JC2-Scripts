e3ba00fcaca7988f46a54a01dae57e5e748cb016
greyday4 <tvinall@gmail.com>
2015-06-11 20:18:47 +0100

    Added Helpmenu

--- InjureME/client/InjureMe.lua
+++ InjureME/client/InjureMe.lua
@@ -4,7 +4,32 @@ function InjureMe:__init()
 	self.version = "0.0.2"
 	self.author = "tally"
 	
+	Events:Subscribe("ModuleLoad", self.ModuleLoad)
+    Events:Subscribe("ModuleUnload", self.ModuleUnload)
+	
 	print("Version " ..tostring(self.version).. " loaded. Written by " ..tostring(self.author).. "for Wham!.")
+end
+
+function InjureMe:ModuleLoad()
+	Events:Fire( "HelpAddItem",
+        {
+            name = "Injure Me",
+            text = 
+                "A basic script with commands to set player and vehicle health.\n\n" ..
+                "To heal yourself type '/healme'.\n" ..
+				"To repair a vehicle type '/healme' while in a vehicle.\n" ..
+				"\nTo damage yourself type '/injureme <value>'.\n" ..
+				"To damage a vehicle type '/injureme <value>' while in a vehicle.\n" ..
+				"\nTo kill yourself type '/killme'.\n" ..
+				"\n:: Written by tally for Wham!"
+        } )
+end
+
+function InjureMe:ModuleUnload()
+    Events:Fire( "HelpRemoveItem",
+        {
+            name = "Injure Me"
+        } )
 end
 
 InjureMe = InjureMe()
--- InjureME/server/InjureMe.lua
+++ InjureME/server/InjureMe.lua
@@ -1,10 +1,10 @@
 class "InjureMe"
 
 function InjureMe:__init()
-	Events:Subscribe("PlayerChat", PlayerChat)
+	Events:Subscribe("PlayerChat", self.PlayerChat)
 end
 
-function PlayerChat(args, player)
+function InjureMe:PlayerChat(args, player)
 	local words = args.text:split(" ")
 	if #words ~= 2 and #words ~= 1 then
 		return
@@ -12,25 +12,33 @@ function PlayerChat(args, player)
  
 	local value = tonumber(words[2])
  
-	if words[1] == "injureme" and value then
+	if words[1] == "/injureme" and value then
 		if value <= 0 or value > 1 then
 			Chat:Send(args.player, "Use a value above 0 and below 1.", Color(255, 0, 255))
 			return false
+		elseif args.player:InVehicle() then
+			local currentvehiclehealth = args.player:GetVehicle():GetHealth()
+			local health = currentvehiclehealth - value
+			args.player:GetVehicle():SetHealth(health)
+			Chat:Send(args.player, "That's gonna cost.", Color(0, 255, 0))
+		return false
 		else
-			local health = 1 - value
+			local currenthealth = args.player:GetHealth()
+			local health = currenthealth - value
 			args.player:SetHealth(health)
 			Chat:Send(args.player, "You hit yourself for " ..tostring(value).. " damage.", Color(255, 0, 0))
 			return false
 		end
-	elseif words[1] == "healme" then
-		args.player:SetHealth(1)
-		Chat:Send(args.player,"Ah, much better!", Color(0, 255, 0))
+	elseif words[1] == "/healme" then
 		if args.player:InVehicle() then
 			args.player:GetVehicle():SetHealth(1)
 			Chat:Send(args.player, "Vehicle Repaired.", Color(0, 255, 0))
 		return false
-		end
-	elseif words[1] == "killme" then
+		else args.player:SetHealth(1)
+			Chat:Send(args.player,"Ah, much better!", Color(0, 255, 0))
+		return false
+	end
+	elseif words[1] == "/killme" then
 		args.player:SetHealth(0)
 		Chat:Send(args.player,"Oops, That was not medicine!", Color(255, 0, 0))
 		return false
